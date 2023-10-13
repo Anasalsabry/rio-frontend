2862fe8bc52d584c661c14788e653f1c09bf1505
Anas Abdu Alsabri <117237832+Anasalsabry@users.noreply.github.com>
2023-10-13 19:24:20 +0300

    Update theme.js

--- components/theme.js
+++ components/theme.js
@@ -31,7 +31,7 @@ const customTheme = createTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#5968bb",
+      main: "#2b2b2b",
       light: "#0288d1",
       dark: "#363d69",
     },
@@ -66,7 +66,7 @@ const adminTheme = createTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#5968bb",
+      main: "#2b2b2b",
       light: "#0288d1",
       dark: "#363d69",
     },
